cd631b86513913882617c93d0c58891371ba41ce
ibobrov <coderilya@gmail.com>
2023-07-28 11:23:44 +1000

    Refactoring UserRepository

--- src/main/java/ru/job4j/cars/repository/UserRepository.java
+++ src/main/java/ru/job4j/cars/repository/UserRepository.java
@@ -7,74 +7,108 @@ import ru.job4j.cars.model.User;
 import java.util.List;
 import java.util.Optional;
 
+import static java.util.Optional.empty;
+
 @AllArgsConstructor
 public class UserRepository {
     private final SessionFactory sf;
 
     public User create(User user) {
-        var session = sf.openSession();
-        session.beginTransaction();
-        session.save(user);
-        session.getTransaction().commit();
-        session.close();
+        final var session = sf.openSession();
+        try (session) {
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
         return user;
     }
 
     public void update(User user) {
-        var session = sf.openSession();
-        session.beginTransaction();
-        session.update(user);
-        session.getTransaction().commit();
-        session.close();
+        final var session = sf.openSession();
+        try (session) {
+            session.beginTransaction();
+            session.update(user);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
     }
 
     public void delete(int userId) {
-        var session = sf.openSession();
-        session.beginTransaction();
-        var user = new User();
-        user.setId(userId);
-        session.delete(user);
-        session.getTransaction().commit();
-        session.close();
+        final var session = sf.openSession();
+        try (session) {
+            session.beginTransaction();
+            var user = new User();
+            user.setId(userId);
+            session.delete(user);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
     }
 
     public List<User> findAllOrderById() {
-        var session = sf.openSession();
-        session.beginTransaction();
-        List<User> result = session.createQuery("FROM User ORDER BY id", User.class).list();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        List<User> rsl = List.of();
+        final var session = sf.openSession();
+        try (session) {
+            session.beginTransaction();
+            rsl = session.createQuery("FROM User ORDER BY id", User.class).list();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
+        return rsl;
     }
 
     public Optional<User> findById(int userId) {
-        var session = sf.openSession();
-        session.beginTransaction();
-        var result = session.get(User.class, userId);
-        session.getTransaction().commit();
-        session.close();
-        return Optional.ofNullable(result);
+        Optional<User> rsl = empty();
+        final var session = sf.openSession();
+        try (session) {
+            session.beginTransaction();
+            rsl = Optional.ofNullable(session.get(User.class, userId));
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
+        return rsl;
     }
 
     public List<User> findByLikeLogin(String key) {
-        var session = sf.openSession();
-        session.beginTransaction();
-        var result = session.createQuery("FROM User WHERE login LIKE :key", User.class)
-                            .setParameter("key", String.format("%%%s%%", key))
-                            .list();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        List<User> rsl = List.of();
+        final var session = sf.openSession();
+        try (session) {
+            session.beginTransaction();
+            rsl = session.createQuery("FROM User WHERE login LIKE :key", User.class)
+                    .setParameter("key", String.format("%%%s%%", key))
+                    .list();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
+        return rsl;
     }
 
     public Optional<User> findByLogin(String login) {
-        var session = sf.openSession();
-        session.beginTransaction();
-        var result = session.createQuery("FROM User WHERE login = :login", User.class)
-                            .setParameter("login", login)
-                            .getSingleResult();
-        session.getTransaction().commit();
-        session.close();
-        return Optional.ofNullable(result);
+        Optional<User> rsl = empty();
+        final var session = sf.openSession();
+        try (session) {
+            session.beginTransaction();
+            rsl = session.createQuery("FROM User WHERE login = :login", User.class)
+                    .setParameter("login", login)
+                    .uniqueResultOptional();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
+        return rsl;
     }
 }
